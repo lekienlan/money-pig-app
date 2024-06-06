b39526d8e0f7b5c2b5578d276d09cd9ca8f8c44a
@lanle98 <lekienlan98@gmail.com>
2024-06-06 23:33:22 +0700

    fix: revert to old

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,8 +1,11 @@
-import 'flavors.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:money_pig/shared/theme/assets.gen.dart';
+import 'package:money_pig/start.dart';
 
-import 'main.dart' as runner;
+import 'flavors.dart';
 
 Future<void> main() async {
   F.appFlavor = Flavor.production;
-  await runner.main();
+  await dotenv.load(fileName: Assets.env.envProduction);
+  await start();
 }
